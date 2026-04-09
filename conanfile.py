20a33fe282d957b5aba48bfc5b51d68397e17a42
Robert Burger <robert.burger@dlr.de>
2026-04-09 06:40:53 +0200

    fix: conan template file

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,11 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[^5.0.6]@robotkernel/stable"
+    python_requires = "conan_template/[~6]@robotkernel/unstable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_dsp402"
     description = "Handling of CiA DSP 402 drive profile."
     exports_sources = ["*", "!.gitignore"]
-    requires = ["robotkernel/[~6]@robotkernel/unstable", ] 
-    
-    def source(self):
-        self.run(f"sed 's/AC_INIT(.*/AC_INIT([{self.name}], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
+    requires = ["robotkernel/[~6]@robotkernel/unstable", ]
 
