1e61709ccd6985853a0f92c174f3cc24bdf42517
Robert Burger <robert.burger@dlr.de>
2026-05-26 08:46:52 +0200

    change: using stable deps

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,11 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~6]@robotkernel/unstable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_dsp402"
     description = "Handling of CiA DSP 402 drive profile."
     exports_sources = ["*", "!.gitignore"]
-    requires = ["robotkernel/[~6]@robotkernel/unstable", ]
+    requires = ["robotkernel/[~6]@robotkernel/stable", ]
 
